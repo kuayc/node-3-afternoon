99b68e756ff669a8700084866ba41db621b02db5
Chin Kuay <kuaychin@gmail.com>
2020-04-02 14:44:55 -0500

    modified productsController.js

--- productsController.js
+++ productsController.js
@@ -1,71 +1,75 @@
-let create = (req, res) => {
-  const dbInstance = req.app.get("db");
-  const { name, description, price, image_url } = req.body;
-
-  dbInstance
-    .createProduct(name, description, price, image_url)
-    .then(() => res.sendStatus(200))
-    .catch(err => {
-      res.status(500).send({ errorMessage: "Something wrong" });
-      console.log(err);
-    });
-};
-
-let getOne = (req, res) => {
-  const dbInstance = req.app.get("db");
-  const { id } = req.params;
-
-  dbInstance
-    .readProduct(id)
-    .then(product => res.status(200).send(product))
-    .catch(err => {
-      res.status(500).send({ errorMessage: "Something wrong" });
-      console.log(err);
-    });
-};
-
-let getAll = (req, res) => {
-  const dbInstance = req.app.get("db");
-
-  dbInstance
-    .readProducts()
-    .then(products => res.status(200).send(products))
-    .catch(err => {
-      res.status(500).send({ errorMessage: "Something wrong" });
-      console.log(err);
-    });
-};
-
-let update = (req, res) => {
-  const dbInstance = req.app.get("db");
-  const { params, query } = req;
-
-  dbInstance
-    .updateProduct(query.desc, params.id)
-    .then(() => res.sendStatus(200))
-    .catch(err => {
-      res.status(500).send({ errorMessage: "Something wrong" });
-      console.log(err);
-    });
-};
-
-let remove = (req, res) => {
-  const dbInstance = req.app.get("db");
-  const { id } = req.params;
-
-  dbInstance
-    .deleteProduct(id)
-    .then(products => res.send(200).send(products))
-    .catch(err => {
-      res.status(500).send({ errorMessage: "Something wrong" });
-      console.log(err);
-    });
-};
-
 module.exports = {
-  create,
-  getOne,
-  getAll,
-  update,
-  remove
+  create: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    const { name, description, price, image_url } = req.body;
+
+    dbInstance
+      .create_product([name, description, price, image_url])
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({
+          errorMessage: "Something wrong."
+        });
+        console.log(err);
+      });
+  },
+
+  getOne: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    const { id } = req.params;
+
+    dbInstance
+      .read_product(id)
+      .then(product => res.status(200).send(product))
+      .catch(err => {
+        res.status(500).send({
+          errorMessage: "Something wrong."
+        });
+        console.log(err);
+      });
+  },
+
+  getAll: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+
+    dbInstance
+      .read_products()
+      .then(products => res.status(200).send(products))
+      .catch(err => {
+        res.status(500).send({
+          errorMessage: "Something wrong."
+        });
+        console.log(err);
+      });
+  },
+
+  update: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    const { params, query } = req;
+
+    dbInstance
+      .update_product([params.id, query.desc])
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({
+          errorMessage: "Something wrong."
+        });
+        console.log(err);
+      });
+  },
+
+  delete: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    const { id } = req.params;
+
+    dbInstance
+      .delete_product(id)
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({
+          errorMessage: "Something wrong."
+        });
+        console.log(err);
+      });
+  }
 };
