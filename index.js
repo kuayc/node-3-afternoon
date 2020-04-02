5158fecda4478dd62c5002f76d4e2b5801284703
Chin Kuay <kuaychin@gmail.com>
2020-04-02 14:35:52 -0500

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
+const products_controller = require("./products_controller");
+
 const app = express();
 
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
@@ -15,6 +17,12 @@ massive({
   .catch(err => console.log(err));
 
 app.use(express.json());
+
+app.post("/api/products", products_controller.create);
+app.get("/api/products", products_controller.getAll);
+app.get("/api/products/:id", products_controller.getOne);
+app.put("/api/products/:id", products_controller.update);
+app.delete("/api/products/:id", products_controller.delete);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}.`);
